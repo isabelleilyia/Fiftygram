f7a7816ce156fd9dd340f94d6d8f8330a47ed322
Isabelle Ilyia <isabelle.ilyia@gmail.com>
2021-09-18 17:00:08 -0400

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,92 @@
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var imageView: UIImageView!
+
+    let context = CIContext()
+    var original: UIImage!
+
+    @IBAction func applySepia() {
+        if original == nil {
+            return
+        }
+
+        let filter = CIFilter(name: "CISepiaTone")
+        filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
+        filter?.setValue(1.0, forKey: kCIInputIntensityKey)
+        display(filter: filter!)
+    }
+
+    @IBAction func applyNoir() {
+        if original == nil {
+            return
+        }
+
+        let filter = CIFilter(name: "CIPhotoEffectNoir")
+        filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
+        display(filter: filter!)
+    }
+
+    @IBAction func applyVintage() {
+        if original == nil {
+            return
+        }
+
+        let filter = CIFilter(name: "CIPhotoEffectProcess")
+        filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
+        display(filter: filter!)
+    }
+
+    @IBAction func applyCircle () {
+        if original == nil {
+            return
+        }
+        let filter = CIFilter(name: "CICircularScreen")
+        filter?.setValue(CIImage(image:original), forKey: kCIInputImageKey)
+        display(filter: filter!)
+        
+    }
+    
+    
+    @IBAction func applyLineOverlay () {
+        if original == nil {
+            return
+        }
+        let filter = CIFilter(name: "CILineOverlay")
+        filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
+        display(filter: filter!)
+    }
+    
+    @IBAction func choosePhoto(_ sender: UIBarButtonItem) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = .photoLibrary
+            self.navigationController?.present(picker, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func savePhoto () {
+        if imageView.image == nil {
+            return
+        }
+        UIImageWriteToSavedPhotosAlbum(imageView.image!, nil, nil, nil)
+    }
+    
+
+    func display(filter: CIFilter) {
+        let output = filter.outputImage!
+        imageView.image = UIImage(cgImage: self.context.createCGImage(output, from: output.extent)!)
+    }
+
+    func imagePickerController(
+        _ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
+    ) {
+        self.navigationController?.dismiss(animated: true, completion: nil)
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = image
+            original = image
+        }
+    }
+}
